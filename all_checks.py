12ec1692ddba2c1f1531acb253b9b88fa0f21643
Edvenan <e_vendrell@yahoo.com>
2022-09-10 00:34:33 +0200

    Corrected invalid syntax

--- all_checks.py
+++ all_checks.py
@@ -13,7 +13,7 @@ def check_disk_full(disk, min_gb, min_precent):
 	du = shutil.disk_usage(disk)
 	# Calculate the percentage of free space
 	percent_free = 100 * du.free / 2**30
-	if percent_free < min_percent or gigabytes_free < min_gb
+	if percent_free < min_percent or gigabytes_free < min_gb:
 		return True
 	return False
 
