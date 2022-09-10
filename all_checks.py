0d6c29b6bfa3d320631bb8642696cd07cfb27f56
Edvenan <e_vendrell@yahoo.com>
2022-09-10 23:41:54 +0200

    Fixed percent_free calculation

--- all_checks.py
+++ all_checks.py
@@ -12,7 +12,7 @@ def check_disk_full(disk, min_gb, min_percent):
 	""" Returns True if there isn't enough disk space, False otherwise. """
 	du = shutil.disk_usage(disk)
 	# Calculate the percentage of free space
-	percent_free = 100 * du.free / 2**30
+	percent_free = 100 * du.free / du.total
 	gigabytes_free = du.free / 2**30
 	if percent_free < min_percent or gigabytes_free < min_gb:
 		return True
